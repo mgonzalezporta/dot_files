50d4c51d86275e614ef2991c525a023c57d8675d
Mar Gonzalez Porta <gonzalezpmdm@gis.a-star.edu.sg>
2020-01-28 11:04:19 +0800

    Set up local machine

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+ln -si `pwd`/dot_bashrc ${HOME}/.bashrc
+ln -si `pwd`/dot_vimrc ${HOME}/.vimrc
+ln -si `pwd`/dot_tmux.conf ${HOME}/.tmux.conf
+ln -si `pwd`/dot_ssh_config ${HOME}/.ssh/config
