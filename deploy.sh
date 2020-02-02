547d5edc8771830aab1785510e82711975c296f8
Mar Gonzalez-Porta <mgonzalezporta@Mars-Air.mshome.net>
2020-02-02 12:12:30 +0800

    keep neovim and vim configs in sync

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 ln -si `pwd`/dot_bashrc ${HOME}/.bashrc
 ln -si `pwd`/dot_vimrc ${HOME}/.vimrc
+ln -si `pwd`/dot_config_nvim_init.vim ${HOME}/.config/nvim/init.vim
 ln -si `pwd`/dot_tmux.conf ${HOME}/.tmux.conf
 ln -si `pwd`/dot_ssh_config ${HOME}/.ssh/config
