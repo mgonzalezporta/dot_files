16db11d422be14bb2ccd380a0d1ba205cb5e5df1
Mar Gonzalez-Porta <mgonzalezporta@gmail.com>
2020-02-02 13:45:11 +0800

    work from nscc

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 ln -si `pwd`/dot_bashrc ${HOME}/.bashrc
 ln -si `pwd`/dot_vimrc ${HOME}/.vimrc
-ln -si `pwd`/dot_config_nvim_init.vim ${HOME}/.config/nvim/init.vim
+if [ -d ${HOME}/.config/nvim/ ]; then ln -si `pwd`/dot_config_nvim_init.vim ${HOME}/.config/nvim/init.vim; fi
 ln -si `pwd`/dot_tmux.conf ${HOME}/.tmux.conf
 ln -si `pwd`/dot_ssh_config ${HOME}/.ssh/config
