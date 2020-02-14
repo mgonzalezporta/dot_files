7c57a745882b7eddae6933c3b2c3046601646e87
Mar Gonzalez-Porta <mgonzalezporta@gmail.com>
2020-02-14 03:37:16 +0000

    updated aws setup

--- setup_aws_ubuntu.sh
+++ setup_aws_ubuntu.sh
@@ -2,6 +2,5 @@
 
 # anaconda
 CONDA_INSTALL_DIR="/home/ubuntu/local/miniconda3"
-alias conda="${CONDA_INSTALL_DIR}/bin/conda"
 source ${CONDA_INSTALL_DIR}/etc/profile.d/conda.sh
 conda activate base
