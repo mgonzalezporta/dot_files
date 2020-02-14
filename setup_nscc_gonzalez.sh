20c73b7302376c23c5d6b43560b05ab613e0cf2c
Mar Gonzalez-Porta <mgonzalezporta@gmail.com>
2020-02-14 11:19:44 +0800

    added setup_nscc_gonzalez.sh

---
+++ setup_nscc_gonzalez.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# aliases
+alias userrig="ssh userrig@gis01"
+
+# anaconda
+module load anaconda/3
+source /data/users/astar/gis/rpd/apps/conda/etc/profile.d/conda.sh
+conda activate mgonzalezporta-nscc
