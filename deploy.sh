9b3bad5cf9be791bc579fddc058291789b8bc72d
Mar Gonzalez-Porta <mgonzalezporta@gmail.com>
2020-02-14 02:49:28 +0000

    add git config

--- deploy.sh
+++ deploy.sh
@@ -5,3 +5,4 @@ ln -si `pwd`/dot_vimrc ${HOME}/.vimrc
 [ -d ${HOME}/.config/nvim/ ] && ln -si `pwd`/dot_config_nvim_init.vim ${HOME}/.config/nvim/init.vim
 ln -si `pwd`/dot_tmux.conf ${HOME}/.tmux.conf
 ln -si `pwd`/dot_ssh_config ${HOME}/.ssh/config
+ln -si `pwd`/dot_gitconfig ${HOME}/.gitconfig
