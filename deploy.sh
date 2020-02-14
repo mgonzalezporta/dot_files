c82b099e61b17b240fc8bfe401345e33f65bb327
Ubuntu <ubuntu@ip-172-31-3-227.ap-southeast-1.compute.internal>
2020-02-14 01:39:35 +0000

    adapt for aws

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,6 @@
 
 ln -si `pwd`/dot_bashrc ${HOME}/.bashrc
 ln -si `pwd`/dot_vimrc ${HOME}/.vimrc
-if [ -d ${HOME}/.config/nvim/ ]; then ln -si `pwd`/dot_config_nvim_init.vim ${HOME}/.config/nvim/init.vim; fi
+[ -d ${HOME}/.config/nvim/ ] && ln -si `pwd`/dot_config_nvim_init.vim ${HOME}/.config/nvim/init.vim
 ln -si `pwd`/dot_tmux.conf ${HOME}/.tmux.conf
 ln -si `pwd`/dot_ssh_config ${HOME}/.ssh/config
