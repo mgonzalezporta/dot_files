d245de602c665cfb4a4415339dd592b650609a58
Mar Gonzalez-Porta <mgonzalezporta@gmail.com>
2020-02-23 16:46:25 +0800

    Update vim set up + NSCC start up tools

--- setup_nscc_gonzalez.sh
+++ setup_nscc_gonzalez.sh
@@ -8,3 +8,6 @@ alias aws="/home/users/astar/gis/gonzalez/local/aws/aws"
 module load anaconda/3
 source /data/users/astar/gis/rpd/apps/conda/etc/profile.d/conda.sh
 conda activate mgonzalezporta-nscc
+
+# singularity
+module load singularity/3.4.0
