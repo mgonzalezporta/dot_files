7c237704221964e0b966a002c76f396f5561e09e
Mar Gonzalez-Porta <mgonzalezporta@gmail.com>
2020-12-12 14:53:58 +0800

    Update setup_nscc_gonzalez.sh

--- setup_nscc_gonzalez.sh
+++ setup_nscc_gonzalez.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # paths
-export PATH=/home/users/astar/gis/gonzalez/local/bin:${PATH}
-export LD_LIBRARY_PATH=${HOME}/local/lib/:${LD_LIBRARY_PATH}
+# export PATH=/home/users/astar/gis/gonzalez/local/bin:${PATH}
+# export LD_LIBRARY_PATH=${HOME}/local/lib/:${LD_LIBRARY_PATH}
 # export LM_LICENSE_FILE=28518@mtr03:28518@mtr02:28518@mtr01
 # export THEANO_FLAGS="base_compiledir=/data/13000026/scratch/gonzalez"
 
@@ -14,11 +14,11 @@ alias qa="qmgr -c 'list queue production' | grep state_count"
 alias qm="qstat | grep '^[0-9]' | awk '{print \$5, \$2}' | sort | uniq -c"
 
 # miniconda
-# module load anaconda/3
-# source /data/users/astar/gis/rpd/apps/conda/etc/profile.d/conda.sh
-# conda activate mgonzalezporta-nscc
-export PATH="/data/13000026/pipeline/dev/gonzalez/tools/miniconda3/bin:$PATH"
-source /data/13000026/pipeline/dev/gonzalez/tools/miniconda3/etc/profile.d/conda.sh
+alias conda="/data/13000026/pipeline/dev/gonzalez/tools/miniconda3-py37/bin/conda"
+source /data/13000026/pipeline/dev/gonzalez/tools/miniconda3-py37/etc/profile.d/conda.sh
 
 # singularity
 module load singularity/3.6.4
+
+# git
+module load git
