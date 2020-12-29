dcc3e7c00cd11b4c2576b751f531a93696ffa575
Mar Gonzalez-Porta <mgonzalezporta@gmail.com>
2020-12-29 01:08:01 +0000

    update set up for aws ubuntu

--- setup_aws_ubuntu.sh
+++ setup_aws_ubuntu.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
-# anaconda
-CONDA_INSTALL_DIR="/home/ubuntu/local/miniconda3"
-source ${CONDA_INSTALL_DIR}/etc/profile.d/conda.sh
-conda activate base
+alias mux="tmux -2u attach || tmux new"
+alias ms="tmux -2u source-file ~/.tmux.conf"
+alias ml="tmux -2u list-sessions"
+alias mk="tmux -2u list-sessions | awk 'BEGIN{FS=\":\"}{print $1}' | xargs -n 1 tmux kill-session -t"
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/jupyter-mgonzalezporta/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/jupyter-mgonzalezporta/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/jupyter-mgonzalezporta/miniconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/jupyter-mgonzalezporta/miniconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
+conda activate mgonzalezporta-dev
