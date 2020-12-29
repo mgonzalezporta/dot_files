017a96f9607683d4c2b4915676741cea6a5a7891
Mar Gonzalez-Porta <mgonzalezporta@gmail.com>
2020-12-29 02:11:24 +0000

    set up aws

--- dot_setup_aws_ubuntu.sh
+++ dot_setup_aws_ubuntu.sh
@@ -7,14 +7,14 @@ alias mk="tmux -2u list-sessions | awk 'BEGIN{FS=\":\"}{print $1}' | xargs -n 1 
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/home/jupyter-mgonzalezporta/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('/home/jupyter-mgonzalezporta/local/miniconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/home/jupyter-mgonzalezporta/miniconda3/etc/profile.d/conda.sh" ]; then
-        . "/home/jupyter-mgonzalezporta/miniconda3/etc/profile.d/conda.sh"
+    if [ -f "/home/jupyter-mgonzalezporta/local/miniconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/jupyter-mgonzalezporta/local/miniconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/home/jupyter-mgonzalezporta/miniconda3/bin:$PATH"
+        export PATH="/home/jupyter-mgonzalezporta/local/miniconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
